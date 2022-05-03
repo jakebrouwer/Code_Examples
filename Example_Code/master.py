368e29b5f1381cd788d2643dde7525f8fccf4c36
jakebrouwer <37711174+jakebrouwer@users.noreply.github.com>
2022-05-03 15:07:10 -0500

    Add files via upload
    added master.py

---
+++ Example_Code/master.py
@@ -0,0 +1,8 @@
+from Example_Code import ProjectEuler, Rosalind
+
+class Master(ProjectEuler,Rosalind):
+    """
+    A purely for fun class to demonstrate class inheritance
+    """
+    def __init__(self):
+        pass
