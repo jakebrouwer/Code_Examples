1999968727d75794f2685da666a4248783d59ba9
jakebrouwer <jakebrouwer25@gmail.com>
2022-05-08 16:26:02 -0500

    added first iteration of optimizer

--- ML_Examples/classical_ml.py
+++ ML_Examples/classical_ml.py
@@ -12,13 +12,18 @@ from sklearn.model_selection import train_test_split
 class ClassicalML:
     
     def __init__(self,data='breast_cancer'):
-        try:
+        self.scores = {}
+        self.best_model = None
+        self.best_parameters = None
+        if isinstance(data,str):
             _data = getattr(datasets,'load_'+data)
             self._data = _data(as_frame=True,return_X_y=True)
             self._X_train,self._X_test,self._y_train,self._y_test = train_test_split(self._data[0],self._data[1])
-            self.scores = {}
-            self.best_model = None
-        except:
+        elif isinstance(data,pd.DataFrame):
+            target_col = data.columns[-1]
+            y = data.pop(target_col)
+            self._X_train,self._X_test,self.y_train,self._y_test = train_test_split(data,y)
+        else:
             raise TypeError('{} could not be loaded. \n\nAvailable datasets are datasets from sklearn.datasets that have the return_X_y parameter'.format(data))
 
         self.models = {
@@ -43,10 +48,36 @@ class ClassicalML:
         for name,model in self.models.items():
             model.fit(self._X_train,self._y_train)
             self.scores[name] = model.score(self._X_test,self._y_test)
-        self.best_model = sorted(self.scores.items(), key= lambda x: x[1],reverse=True)
+        self.best_model = sorted(self.scores.items(), key= lambda x: x[1],reverse=True)[0]
         return self.best_model
 
-    def optimize_best_model(self):
-        pass
+    def optimize_best_model(self,n_trials:int = 100,categorical_suggestions:dict=None,float_suggestions:dict=None,int_suggestions:dict=None):
+        import optuna
+        from sklearn.metrics import accuracy_score
+        if not self.best_model:
+            raise AttributeError('A model has not been generated.\nRun find_best_model() to generate a model.\n\n')
+        model = self.models[self.best_model[0]]
 
+        def objective(trial):
+            opt_params={}
+
+            if categorical_suggestions:
+                for key in categorical_suggestions.keys():
+                    opt_params[key] = trial.suggest_categorical(key,categorical_suggestions[key])
+            if float_suggestions:
+                for key in float_suggestions.keys():
+                    opt_params[key] = trial.suggest_float(key,**float_suggestions[key])
+            if int_suggestions:
+                for key in int_suggestions.keys():
+                    opt_params[key] = trial.suggest_int(key,**int_suggestions[key])
+
+            if not opt_params == {}:
+                model.set_params(**opt_params)
+            pred = model.predict(self._X_test)
+            return accuracy_score(self._y_test,pred)
+
+        study = optuna.create_study(direction='maximize')
+        study.optimize(objective,n_trials=n_trials)
+        trial = study.best_trial
+        self.best_parameters = trial.params
 
